232b127cd1aefde5927113b119fb562cb343b9b1
jtaka1012 <simple.gadget.life@gmail.com>
2018-09-27 15:28:58 +0900

    通知名称を変更

--- NextGrowingTextView/NextGrowingInternalTextView.swift
+++ NextGrowingTextView/NextGrowingInternalTextView.swift
@@ -35,7 +35,7 @@ internal class NextGrowingInternalTextView: UITextView {
   override init(frame: CGRect, textContainer: NSTextContainer?) {
     super.init(frame: frame, textContainer: textContainer)
 
-    NotificationCenter.default.addObserver(self, selector: #selector(NextGrowingInternalTextView.textDidChangeNotification(_ :)), name: UITextView.textDidChangeNotification, object: self)
+    NotificationCenter.default.addObserver(self, selector: #selector(NextGrowingInternalTextView.textDidChangeNotification(_ :)), name: .UITextViewTextDidChange, object: self)
   }
 
   required init?(coder aDecoder: NSCoder) {
